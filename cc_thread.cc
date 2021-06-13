ea5567352b50f9dcbabb2183854afc130e8e8203
hiraieject <hirai@eject.org>
2021-06-13 20:46:13 +0900

    .

---
+++ cc_thread.cc
@@ -0,0 +1,144 @@
+#include "cc_thread.h"
+
+//#define CC_THREAD_DBGPR
+
+#if !defined(CC_THREAD_DBGPR) || !defined(__linux)
+#undef CC_THREAD_DBGPR
+#undef CC_THREAD_ERRPR
+#undef CC_THREAD_WARNPR
+#define CC_THREAD_DBGPR(fmt, args...)
+#define CC_THREAD_ERRPR(fmt, args...)
+#define CC_THREAD_WARNPR(fmt, args...)
+
+#else 
+#undef CC_THREAD_DBGPR
+#undef CC_THREAD_ERRPR
+#undef CC_THREAD_WARNPR
+#define CC_THREAD_DBGPR(fmt, args...)	\
+	{ printf("[%s:%s():%d] " fmt, __FILE__,__FUNCTION__,__LINE__,## args); }
+#define CC_THREAD_ERRPR(fmt, args...) \
+	{ printf("[%s:%s():%d] ##### ERROR!: " fmt, __FILE__,__FUNCTION__,__LINE__, ## args); }
+#define CC_THREAD_WARNPR(fmt, args...)											\
+	{ printf("[%s:%s():%d] ##### WARNING!: " fmt, __FILE__,__FUNCTION__,__LINE__, ## args); }
+#endif
+
+// =====================================================================================
+// ===================================================================================== THREAD
+// =====================================================================================
+
+void *
+cc_thread::thread_func_template (void *arg)
+{
+	//　以下テンプレート、継承先のクラスでは　thread_func() で定義する
+
+	cc_thread *myobj = (cc_thread *)arg;
+	struct timeval timeout;
+	fd_set	rfds;
+
+	while (myobj->thread_loop) {
+        // タイムアウト値の設定
+        timeout.tv_sec	= 0;
+		timeout.tv_usec	= 200*1000;
+
+		FD_ZERO(&rfds);
+		FD_SET(myobj->fifo_to_thread->get_fd(), &rfds);
+		select(FD_SETSIZE, &rfds, 0, 0, &timeout);		// wait
+
+		if( FD_ISSET(myobj->fifo_to_thread->get_fd(), &rfds) ) {
+			int recvsize;
+			unsigned char buf[100];
+			recvsize = myobj->fifo_to_thread->recv (buf, sizeof(buf));
+			printf ("recv fifo event from main (%dbyte)\n", recvsize);
+		}
+	}
+	pthread_exit (NULL);
+	return NULL;
+}
+
+// -------------------------------------------------------------------------------------------
+
+cc_thread::cc_thread (bool thread_up_flg, void * (*start_routine)(void *))
+{
+	CC_THREAD_DBGPR ("cc_thread: instance created\n");
+
+	// initialize
+	thread_id			= 0;;
+	thread_loop			= false;
+
+	// create fifo
+	fifo_to_thread    = new cc_fifo ();
+	fifo_from_thread  = new cc_fifo ();
+	
+	// thread
+	if (thread_up_flg) {
+		thread_up (start_routine);
+	}
+}
+
+cc_thread::~cc_thread ()
+{
+	thread_down ();
+
+	delete fifo_to_thread;
+	delete fifo_from_thread;
+
+	CC_THREAD_DBGPR ("cc_thread: instance deleted\n");
+}
+
+// -------------------------------------------------------------------------------------------
+
+bool
+cc_thread::thread_up (void * (*start_routine)(void *))
+{
+	if (thread_id == 0) {
+		// wakeup thread
+		thread_loop = true;
+		if (pthread_create (&thread_id, NULL, start_routine, (void*)this) != 0) {
+			thread_loop = false;
+			perror("cc_thread: pthread_create() error");
+			return false;
+		}
+		// joinするので、detachしない
+		//pthread_detach (thread_id);
+	}
+	return true;
+}
+
+bool
+cc_thread::thread_down(void)
+{
+	if (thread_id != 0) {
+		thread_loop = false;
+
+		CC_THREAD_DBGPR ("start pthread join\n");
+		pthread_join (thread_id, NULL);
+		CC_THREAD_DBGPR ("success pthread join\n");
+
+		thread_id = 0;
+	}
+	return true;
+}
+
+// -------------------------------------------------------------------------------------------
+
+int
+cc_thread::send_to_thread (void *dptr, int dsize)
+{
+	return fifo_to_thread->send (dptr, dsize);
+}
+int
+cc_thread::recv_to_thread (void *bptr, int bsize)
+{
+	return fifo_to_thread->recv (bptr, bsize);
+}
+
+int
+cc_thread::send_from_thread (void *dptr, int dsize)
+{
+	return fifo_from_thread->send (dptr, dsize);
+}
+int
+cc_thread::recv_from_thread (void *bptr, int bsize)
+{
+	return fifo_from_thread->recv (bptr, bsize);
+}
---
+++ cc_thread.h
@@ -0,0 +1,58 @@
+/* -*- Mode: C; tab-width: 4; c-basic-offset: 4 -*- */
+
+/**
+ * @file wb_fifo.h
+ * @brief thread class for C++  by hirai
+ */
+
+#ifndef __CC_THREAD_H__
+#define __CC_THREAD_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+#include "cc_fifo.h"
+
+///
+/// ■■■■■　thread 制御クラス
+///
+class cc_thread {
+private:
+public:
+	// ---------------------------------------------------------
+	pthread_t thread_id;							/// ■■■■■　スレッドID
+	bool thread_loop;								/// ■■■■■　スレッドループ有効フラグ、falseにするとスレッドループを抜ける
+
+	cc_fifo *fifo_to_thread;						/// ■■■■■　メインからスレッド向けのFIFO
+	cc_fifo	*fifo_from_thread;						/// ■■■■■　スレッドからメイン向けのFIFO
+
+	// ---------------------------------------------------------
+	cc_thread (bool thread_up_flg, void * (*start_routine)(void *)=thread_func_template);/// ■■■■■　コンストラクタ
+	virtual ~cc_thread ();							/// ■■■■■　デストラクタ
+
+	static void *thread_func_template (void *arg);	/// ■■■■■　再定義用スレッド関数（継承先で再定義すること）
+	virtual bool thread_up (void * (*start_routine)(void *)); /// ■■■■■　スレッド起動
+	virtual bool thread_down (void);				/// ■■■■■　スレッド停止
+
+	int send_to_thread (void *dptr, int dsize);		/// ■■■■■　メインからスレッドにデータ送信
+	int recv_to_thread (void *bptr, int bsize);		/// ■■■■■　メインからのデータをスレッドで受信
+
+	int send_from_thread (void *dptr, int dsize);	/// ■■■■■　スレッドからメインにデータ送信
+	int recv_from_thread (void *bptr, int bsize);	/// ■■■■■　スレッドからのデータをメインで受信
+};
+
+#endif// __CC_THREAD_H__
+
